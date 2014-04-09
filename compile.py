f2076ff340dd5bc9b6ef3f3338c9f00c13447696
James Atwood <jatwood@cs.umass.edu>
2014-04-09 14:22:42 +0900

    improved compiler

--- compile.py
+++ compile.py
@@ -11,7 +11,9 @@ def compile(filename):
     raw_string = f.read().replace('\n',' ')
     raw_list = [x for x in raw_string.split(' ') if x != '']
     parsed_list = []
+    #for i in xrange(1,len(raw_list)):
     for item in raw_list:
+        #item = raw_list[i]
         if len(item) > 1 and item[:2] == 'OP':
             try:
                 parsed_list.append(ops.word[item])
@@ -19,7 +21,14 @@ def compile(filename):
                 print "Unrecognized op %s" % (item,)
                 return
         else:
-            parsed_list.append(bytestream.bytestream(item))
+            bstream = bytestream.bytestream(item)
+            # add a pushing op if needed
+            if (len(parsed_list) == 0 or
+                ((isinstance(parsed_list[-1], ops.op) and (parsed_list[-1].opcode < 1 or parsed_list[-1].opcode > 75)) or
+                not isinstance(parsed_list[-1], ops.op))):
+                parsed_list.append(ops.code[len(bstream)])
+            parsed_list.append(bstream)
+
     stream = bytestream.bytestream('')
     for parsed_item in parsed_list:
         if(isinstance(parsed_item, ops.op)):
